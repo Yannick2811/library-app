fb6fd3f176d179c6e567981dd251f95233a06798
Yannick2811 <yanschen90@gmail.com>
2022-01-07 15:52:03 +0100

    Add index.html and style.css and script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let titleInput = document.querySelector("#input-title");
+let authorInput = document.querySelector("#input-author");
+let pagesInput = document.querySelector("#input-pages");
+let readInput = document.querySelector("#input-read");
+const btnSubmit = document.querySelector("#submit");
+const form = document.querySelector("#form-book");
+
 // Book constructor
 class Book {
   constructor(title, author, pages, read) {
@@ -7,64 +14,79 @@ class Book {
     this.read = read;
   }
 }
-
-// Library
-const myLibrary = [];
+let myLibrary = [];
 let newBook;
 
-// add a book to the Library
-function addBookToLibrary() {
-  newBook = new Book(title, author, pages, read);
-  myLibrary.push(newBook);
-}
-const btnSubmit = document.querySelector("#submit");
-btnSubmit.addEventListener("click", addBookToLibrary());
-console.log(myLibrary);
-
-// add book to DOM and display
-function displayBook() {
-  const display = document.querySelector("#book-list");
-  const books = document.querySelectorAll(".book");
-  books.forEach((book) => display.removeChild(book));
-
-  for (let i = 0; i < myLibrary.length; i++) {
-    createBook(myLibrary[i]);
-  }
-}
-
-//creates the book to be displayed
-function createBook(item) {
-  const library = document.querySelector("#book-list");
-  const divBook = document.createElement("div");
-  const divTitle = document.createElement("div");
-  const divAuthor = document.createElement("div");
-  const divPages = document.createElement("div");
-  const btnRead = document.createElement("button");
-  const btnRemove = document.createElement("button");
-
+function addBook(i) {
+  const bookList = document.querySelector("#book-list");
+  let divBook = document.createElement("div");
   divBook.classList.add("book");
-  divBook.setAttribute("id", myLibrary.indexOf(item));
+  divBook.setAttribute("data-index", `${i}`);
 
-  divTitle.textContent = item.title;
-  divTitle.classList.add("title");
-  divBook.appendChild(divTitle);
+  const title = titleInput.value;
+  let divTitle = document.createElement("div");
+  divTitle.textContent = `Title: ${title}`;
 
-  divAuthor.textContent = item.author;
-  divAuthor.classList.add("author");
-  divBook.appendChild(divAuthor);
+  const author = authorInput.value;
+  let divAuthor = document.createElement("div");
+  divAuthor.textContent = `Author: ${author}`;
 
-  divPages.textContent = item.pages;
-  divPages.classList.add("pages");
-  divBook.appendChild(divPages);
+  const pages = pagesInput.value;
+  let divPages = document.createElement("div");
+  divPages.textContent = `Pages: ${pages}`;
 
-  btnRead.classList.add("btnRead");
-  divBook.appendChild(btnRead);
-  if (item.read.value === "Read") {
+  const read = readInput.value;
+  let btnRead = document.createElement("button");
+  if (read == "read") {
     btnRead.textContent = "Read";
-    btnRead.style.backgroundColor = "#1aff00";
+    btnRead.style.backgroundColor = "#00ff44";
   } else {
     btnRead.textContent = "Not Read";
-    btnRead.style.backgroundColor = "#ff0008";
+    btnRead.style.backgroundColor = "#ff0015";
   }
-  library.appendChild(divBook);
+  const btnDelete = document.createElement("button");
+  btnDelete.textContent = "Delete";
+
+  divBook.appendChild(divTitle);
+  divBook.appendChild(divAuthor);
+  divBook.appendChild(divPages);
+  divBook.appendChild(btnRead);
+  divBook.appendChild(btnDelete);
+  bookList.appendChild(divBook);
+  newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  btnDelete.addEventListener("click", () => {
+    bookList.removeChild(divBook);
+    myLibrary.splice(divBook, 1);
+  });
+  btnRead.addEventListener("click", () => {
+    if (btnRead.textContent === "Read") {
+      btnRead.textContent = "Not Read";
+      btnRead.style.backgroundColor = "#ff0015";
+    } else {
+      btnRead.textContent = "Read";
+      btnRead.style.backgroundColor = "#00ff44";
+    }
+  });
 }
+
+btnSubmit.addEventListener("click", (e, i) => {
+  if (
+    titleInput.value === "" ||
+    authorInput.value === "" ||
+    pagesInput.value === ""
+  ) {
+    alert("Please input title, author and page number!");
+  } else {
+    e.preventDefault();
+    addBook(i);
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+  }
+});
+window.onload = function resetForm() {
+  titleInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
+};
